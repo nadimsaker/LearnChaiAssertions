62d9f4d012bbb57f6e5caafa373d00ead6f187f0
Nadim Saker <nadimsaker@gmail.com>
2019-10-10 15:10:46 +0600

    deep equals

--- test/chai.js
+++ test/chai.js
@@ -19,3 +19,12 @@ expect(x).to.be.an("object");
 //expect(x).to.be.equals(y);
 
 expect(x).to.be.deep.equals(y, "x and y are equal.");
+
+expect(x)
+  .to.be.an("object")
+  .and.to.be.deep.equals(y, "x and y are equal.");
+
+let number = [1, 2, 3];
+expect(number)
+  .to.be.an("array")
+  .and.to.include(4);
